227fe711109580adae0b0112ba5a9152ca7b20e9
gtsiamp <gtsiamp@gmail.com>
2016-07-23 17:21:58 +0300

    Remove layers backup script cause Substratum will handle this.

--- products/vendorless.mk
+++ products/vendorless.mk
@@ -5,6 +5,4 @@
     vendor/krexus/prebuilt/common/vendor/media/LMprec_508.emd:system/vendor/media/LMprec_508.emd \
     vendor/krexus/prebuilt/common/vendor/media/PFFprec_600.emd:system/vendor/media/PFFprec_600.emd
 
-# Layers Backup
-PRODUCT_COPY_FILES += \
-    vendor/krexus/prebuilt/common/addon.d/71-layers.sh:system/addon.d/71-layers.sh
+
