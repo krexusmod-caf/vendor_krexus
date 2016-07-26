79e18df3439cebcaae6035617048129bdd47c8a5
gtsiamp <gtsiamp@gmail.com>
2016-07-27 02:19:55 +0300

    Add krexusmod version

--- products/common.mk
+++ products/common.mk
@@ -16,7 +16,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.setupwizard.network_required=false \
     ro.setupwizard.gservices_delay=-1 \
     ro.adb.secure=1 \
-    ro.krexus.version=krexus_mm-caf-$(shell date +"%y%m%d")-$(TARGET_DEVICE)
+    ro.krexus.version=krexus_mm-caf-$(shell date +"%y%m%d")-$(TARGET_DEVICE) \
+    ro.krexusmod.version=krexusmod_mm-caf-$(shell date +"%y%m%d")-$(TARGET_DEVICE)
 
 # Thank you, please drive thru!
 PRODUCT_PROPERTY_OVERRIDES += persist.sys.dun.override=0
